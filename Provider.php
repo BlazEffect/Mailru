0211ca3ebdd4cc9e53380f55e4916565fb39923c
BlazEffect <kamalinigor@gmail.com>
2024-04-02 17:55:42 +0300

    Fixed errors with obtaining access token and authorization

--- Provider.php
+++ Provider.php
@@ -2,18 +2,12 @@
 
 namespace SocialiteProviders\Mailru;
 
-use GuzzleHttp\RequestOptions;
-use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 
 class Provider extends AbstractProvider
 {
     const IDENTIFIER = 'MAILRU';
-
-    /**
-     * {@inheritdoc}
-     */
-    protected $scopes = ['userinfo'];
 
     /**
      * {@inheritdoc}
@@ -36,13 +30,15 @@ class Provider extends AbstractProvider
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get('https://oauth.mail.ru/userinfo', [
-            RequestOptions::QUERY => [
-                'access_token' => $token,
-            ],
-        ]);
+        $params = [
+            'access_token' => $token,
+        ];
 
-        return json_decode((string) $response->getBody(), true);
+        $response = $this->getHttpClient()->get(
+            'https://oauth.mail.ru/userinfo?' . http_build_query($params)
+        );
+
+        return json_decode($response->getBody(), true);
     }
 
     /**
@@ -56,6 +52,27 @@ class Provider extends AbstractProvider
             'name'     => $user['name'],
             'email'    => $user['email'],
             'avatar'   => $user['image'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCodeFields($state = null)
+    {
+        return array_merge(parent::getTokenFields($state), [
+            'state' => 'ignored_string',
+            'response_type' => 'code',
         ]);
     }
 }
